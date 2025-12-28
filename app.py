ffdad25652baeb39f230197bac59ac9d4feac0f5
afifanusrat3 <afifanusrat3@gmail.com>
2025-12-28 16:16:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ for i in X:
 scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)
 final_value = scaler.transform([all_value])
-model = RandomForestRegressor()
+model = st.cache(RandomForestRegressor)()
 model.fit(X,y)
 house_price = model.predict(final_value)[0][0]
 
@@ -36,6 +36,7 @@ st.success(msg)
 
 
 st.markdown('''**Designed and Developed by: Afifa Nusrat**''')  
+
 
 
 
