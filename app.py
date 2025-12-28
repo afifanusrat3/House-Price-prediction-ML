172c5d6c31bd29bb23a0b53e974aa61b582f357e
afifanusrat3 <afifanusrat3@gmail.com>
2025-12-28 14:59:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
 st.title('🏠House Price prediction using ML')
+st.image('https://i.pinimg.com/170x/58/8b/50/588b50271e33d25e9b7d894acf3518bc.jpg')
+
 
